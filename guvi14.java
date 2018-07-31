5973aa63a79c46a0b8f2830b959fbbe3ecf752de
anushabollineni <41730220+anushabollineni@users.noreply.github.com>
2018-07-31 19:51:14 +0530

    Create guvi14.java

---
+++ guvi14.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("The Odd Numbers are:");
+		for (int i = 1; i <= 6; i++)
+		{
+			if (i % 2 != 0)
+			{
+				System.out.print(i + " ");
+		}
+		}}
+}
